9d5a8a22af26e08ad555380c8963aae878c57ba8
Daniel Luzz <dlrandrade@gmail.com>
2025-07-06 00:10:54 -0300

    Update index.ts

--- api/users/index.ts
+++ api/users/index.ts
@@ -4,17 +4,33 @@ const prisma = new PrismaClient()
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { username, password, role = 'user' } = req.body
+    const { name, email, password, image = null, role = 'user' } = req.body
 
     try {
-      const user = await prisma.user.create({
-        data: { username, password, role },
+      // Verifica se o usuário já existe
+      const existingUser = await prisma.user.findUnique({
+        where: { email }
       })
+
+      if (existingUser) {
+        return res.status(409).json({ error: 'Usuário já existe' })
+      }
+
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password,
+          image,
+          role,
+        },
+      })
+
       return res.status(201).json(user)
     } catch (error) {
-  console.error(error); // importante para Vercel logar
-  return res.status(500).json({ error: error.message || 'Erro desconhecido' });
-}
+      console.error(error)
+      return res.status(500).json({ error: error.message || 'Erro interno' })
+    }
   } else {
     return res.status(405).json({ error: 'Método não permitido' })
   }
